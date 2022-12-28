437c719c7c090aec694adc40bceeb9920cd908a0
unknown <mohamed101291@gmail.com>
2022-12-28 15:24:48 +0200

    files update

---
+++ src/routes/tests/productSpec.ts
@@ -0,0 +1,48 @@
+import userModel from '../../models/user.model';
+import client from '../../database';
+import User from '../../types/user.type';
+
+const userStore = new userModel();
+
+describe('UserModels', () => {
+  describe('Test Methods', () => {
+    it('Get all users method', () => {
+      expect(userStore.index).toBeDefined();
+    });
+    it('Get one user method', () => {
+      expect(userStore.show).toBeDefined();
+    });
+    it('create one user method', () => {
+      expect(userStore.create).toBeDefined();
+    });
+    it('Update one user method', () => {
+      expect(userStore.update).toBeDefined();
+    });
+    it('Delete one user method', () => {
+      expect(userStore.delete).toBeDefined();
+    });
+  });
+  describe('Test logic', () => {
+    const user = {
+      firstName: 'test',
+      lastName: 'user',
+      userName: 'mohamed1234',
+      password: 'test123',
+    } as User;
+
+    beforeAll(async () => {
+      const createTestUser = await userStore.create(user);
+      user.usersID = createTestUser.usersID;
+    });
+    afterAll(async () => {
+      const conn = await client.connect();
+      const sql = `DELETE FROM users;`;
+      await conn.query(sql);
+      conn.release();
+    });
+    it('Get all users should return all users in the database', async () => {
+      const users = await userStore.index();
+      expect(users.length).toBe(1);
+    });
+  });
+});
